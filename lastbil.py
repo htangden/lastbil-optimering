764d2be72f98acba9860f11531c660c884dbe7b5
Hugo Tängdén <htangden@gmail.com>
2025-04-01 18:52:30 +0200

    kombinationer av grossist för mellanlager

--- lastbil.py
+++ lastbil.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-from pulp import LpVariable, LpProblem, LpMinimize, LpInteger, lpSum, value
+from pulp import LpVariable, LpProblem, LpMinimize, LpInteger, lpSum, value, PULP_CBC_CMD
 from geopy.distance import geodesic
+from itertools import combinations
 from typing import Tuple
 import math
 import sys
 
-TRAIN_TO_TRUCK = 5
+TRAIN_TO_TRUCK = 10
 
 
 class Building:
@@ -18,6 +19,9 @@ class Building:
         return geodesic(self.pos, other.pos).kilometers
     
     def __str__(self):
+        return self.name
+    
+    def __repr__(self):
         return self.name
     
 
@@ -33,10 +37,6 @@ class Supplier(Building):
         super().__init__(pos, name)
         self.needed = needed
 
-class Distributor(Building):
-
-    def __init__(self, pos : Tuple[float, float], name : str):
-        super().__init__(pos, name)
  
 
 def weighted_midpoint(suppliers : list[Supplier], factories : list[Factory]) -> Tuple[float, float]:
@@ -53,15 +53,12 @@ def weighted_midpoint(suppliers : list[Supplier], factories : list[Factory]) -> 
     y = ak2_yk/ak2
     return (x, y)
 
-model = LpProblem("lastbilsproblem ", LpMinimize)
 
 
-path_to_data = sys.argv[1]
 
-
+#Create factories, suppliers from file
 factories, suppliers = [], []
-
-with open(path_to_data) as file:
+with open(sys.argv[1]) as file:
     section = None
     for line in file:
         line = line.strip()
@@ -78,49 +75,62 @@ with open(path_to_data) as file:
             )
 
 
-suppliers.append(Supplier(weighted_midpoint(suppliers, factories), "Mellanlager", 0))
+def run_with_midpoint(suppliers, factories, midpoint_pos):
+
+    model = LpProblem(f"lastbilsproblem {name}", LpMinimize)
+
+    suppliers_with_midpoint = suppliers + [Supplier(midpoint_pos, "Mellanlager", 0)]
+
+    road_len = []
+    num_trucks = []
+
+    for i, building in enumerate(factories + suppliers_with_midpoint):
+        num_trucks.append([])
+        road_len.append([])
+        for j, supplier in enumerate(suppliers_with_midpoint):
+            num_trucks[i].append(LpVariable(f"LASTBILAR:{building}_{supplier}", 0, None, LpInteger))
+            road_len[i].append(building.distanceTo(supplier))
 
 
-road_len = []
-num_trucks = []
+    for row in road_len:
+        row[-1] /= TRAIN_TO_TRUCK
 
-for i, building in enumerate(factories + suppliers):
-    num_trucks.append([])
-    road_len.append([])
-    for j, supplier in enumerate(suppliers):
-        num_trucks[i].append(LpVariable(f"LASTBILAR:{building}_{supplier}", 0, None, LpInteger))
-        road_len[i].append(building.distanceTo(supplier))
+    model += lpSum(road_len[i][j] * num_trucks[i][j] for i in range(len(factories + suppliers_with_midpoint)) for j in range(len(suppliers_with_midpoint)))
 
 
+    for i, building in enumerate(factories):
+        model += lpSum(truck_from_factory for truck_from_factory in num_trucks[i]) <= building.production_quantity, f"Lastbilsbegränsning {building}"
 
-for row in road_len:
-    row[-1] /= TRAIN_TO_TRUCK
-
-model += lpSum(road_len[i][j] * num_trucks[i][j] for i in range(len(factories + suppliers)) for j in range(len(suppliers)))
-
-
-for i, building in enumerate(factories):
-    model += lpSum(truck_from_factory for truck_from_factory in num_trucks[i]) <= building.production_quantity, f"Lastbilsbegränsning {building}"
-
-for i, supplier in enumerate(suppliers):
-    trucks_to_supplier = [row[i] for row in num_trucks]
-    sum_to_supplier = lpSum(truck_to_supplier for truck_to_supplier in trucks_to_supplier)
-    sum_from_supplier = lpSum(truck_from_supplier for truck_from_supplier in num_trucks[len(factories) + i])
-    model += (sum_to_supplier - sum_from_supplier) == supplier.needed, f"Rätt mängd till {supplier}"
+    for i, supplier in enumerate(suppliers_with_midpoint):
+        trucks_to_supplier = [row[i] for row in num_trucks]
+        sum_to_supplier = lpSum(truck_to_supplier for truck_to_supplier in trucks_to_supplier)
+        sum_from_supplier = lpSum(truck_from_supplier for truck_from_supplier in num_trucks[len(factories) + i])
+        model += (sum_to_supplier - sum_from_supplier) == supplier.needed, f"Rätt mängd till {supplier}"
 
 
-model.writeLP("lastbil.lp")
-model.solve()
+    model.writeLP("lastbil.lp")
+    model.solve(PULP_CBC_CMD(msg=False))
+    return model
+
+
+models = []
+
+for r in range(1, len(suppliers)):
+    for supplier_combo in combinations(suppliers, r):
+        midpoint = weighted_midpoint(list(supplier_combo), factories)
+        models.append(run_with_midpoint(suppliers, factories, midpoint))
+        print(supplier_combo, value(models[-1].objective))
+
+best_model = min(models, key=lambda x: value(x.objective))
 
 
 with open("solution.txt", "w") as f:
     f.write("OPTIMALA VÄRDEN:\n")
-    for v in model.variables():
+    for v in best_model.variables():
         if (v.varValue != 0):
             f.write(f"{v.name} = {v.varValue}\n")
     f.write(f"\nPOSITION MELLANLAGER: ({suppliers[-1].pos[0]:.2f}, {suppliers[-1].pos[1]:.2f}) ")
-    f.write(f"\nSTRÄCKA KÖRD: {value(model.objective):.2f}")
-
+    f.write(f"\nSTRÄCKA KÖRD: {value(best_model.objective):.2f}")
 
 
 
